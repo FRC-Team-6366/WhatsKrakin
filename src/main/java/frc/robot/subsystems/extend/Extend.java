d9a2cca3e818499edd763415f34a0bdda51e70a1
FRC6366RamRodz <132411530+FRC6366RamRodz@users.noreply.github.com>
2025-03-17 08:35:47 -0400

    yes

--- src/main/java/frc/robot/subsystems/extend/Extend.java
+++ src/main/java/frc/robot/subsystems/extend/Extend.java
@@ -5,9 +5,9 @@ import frc.robot.subsystems.extend.ExtendIO.ExtendIOInputs;
 
 
 public class Extend {
-        private ExtendIO io;
+    private ExtendIO io;
     private ExtendIOInputs inputs = new ExtendIOInputsAutoLogged();
-  
+
     public Extend(ExtendIO io) {
       this.io = io;
     }
@@ -15,12 +15,12 @@ public class Extend {
     public void moveExtend(double rotations){
         io.moveExtend(rotations);
     }
-    
+
     public boolean extendAtSetPoint(double atPosition){
       return io.extendAtSetPoint(atPosition);
-      }
+    }
 
-  
+
     /** updates extend values periodically */
     public void periodic() {
       io.updateInputs(inputs);
--- src/main/java/frc/robot/subsystems/extend/ExtendConstants.java
+++ src/main/java/frc/robot/subsystems/extend/ExtendConstants.java
@@ -12,13 +12,13 @@ public final class ExtendConstants {
     public static double extendSupplyCurrentLimits = 40;
     public static boolean extendCurrentLimitEnabled = true;
 
-    public static double extendkP = 15; //
-    public static double extendkI = 0; // 0
-    public static double extendkD = 0; // 0.5
+    public static double extendkP = 15; //15
+    public static double extendkI = 0; //0
+    public static double extendkD = 0; //0
     public static double extendkS = 0; //0
-    public static double extendkV = 0; // 0.12
-    public static double extendkA = 0; //0.01
-    public static double extendkG = 0; // 0
+    public static double extendkV = 0; //0
+    public static double extendkA = 0; //0
+    public static double extendkG = 0; //0
     public static double extendSoftLimitLow = 0; // Not used
     public static double extendSoftLimitHigh = 0; // Not used
     public static double extendSensorRatio = 1;
@@ -28,5 +28,3 @@ public final class ExtendConstants {
     public static double extendMMAcceleration = 11;
     public static double extendMMJerk = 100;
 }
-    
-
